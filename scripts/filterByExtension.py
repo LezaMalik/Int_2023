f5e1ff90672a9e063a9d43d8d931c3da43c73f52
Leza Malik <93573683+LezaMalik@users.noreply.github.com>
2023-08-09 08:59:05 +0500

    Update filterByExtension.py

--- scripts/filterByExtension.py
+++ scripts/filterByExtension.py
@@ -1,7 +1,7 @@
 # This code reads a CSV file containing information about files and their sizes. 
 # It calculates statistics about file extensions and presents the data in a formatted table. 
-# The table display 5 columns
-# Extension Type, Total File Size, Number of Files, Size % per extension, % Count per extension
+# The table display 5 columns in a well formatted form
+# Columns are: Extension Type, Total File Size, Number of Files, Size % per extension, % Count per extension
 # The user is given the option to save the calculated data to a new CSV file.
 # Usage: python3 myscript.py <input_csv_file>
 
