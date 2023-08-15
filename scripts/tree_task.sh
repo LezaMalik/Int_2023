b8201b2db616256fa57a1db78f98159e2490dbda
Leza Malik <93573683+LezaMalik@users.noreply.github.com>
2023-08-15 23:34:32 +0500

    Update tree_task.sh

--- scripts/tree_task.sh
+++ scripts/tree_task.sh
@@ -3,6 +3,7 @@
 # in pwd and its subdirectories including details
 # files permissions, file owner, filesize in bytes, file last modified date (in the format YYYY-MM-DD), filename with abs path, filename
 # and saves it in a csv (comma-separated file)
+# Usage: ./tree_task.sh <absolute-path>
 
 find "$1" -type f -exec ls -lh --time-style=long-iso --block-size=1 {} + | awk '{split($6, a, "-"); print $1 " , " $3 " , " $5 " , " a[1]"-"a[2]"-"a[3] " , " $NF }' > treefile.csv
 awk -F ' , ' 'BEGIN { OFS=" , " } { split($NF, path_arr, "/"); print $0, path_arr[length(path_arr)] }' treefile.csv > tree_data.csv
